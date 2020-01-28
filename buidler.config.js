20ecdc15232991f4d1249ab85e98f1b7414bd1ec
Daniel Nolan <DanNolan99@gmail.com>
2020-01-27 23:27:33 -0500

    first commit

---
+++ buidler.config.js
@@ -0,0 +1,8 @@
+usePlugin("@nomiclabs/buidler-truffle5");
+
+module.exports = {
+    defaultNetwork: "buidlerevm",
+    mocha: {
+        // reporter: require("./mocha-reporter"),
+    },
+};
---
+++ contracts/Escrow.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+contract EscrowContract {
+	address public arbiter;
+	address public beneficiary;
+	address public depositor;
+}
